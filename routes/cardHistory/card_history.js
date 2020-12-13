065245ccd37be885364745c8aa67e3f295f1fdf3
VivaanShin <xhatmtjfla23@gmail.com>
2020-12-13 16:37:25 +0900

    yj 12131637 week test

--- routes/cardHistory/card_history.js
+++ routes/cardHistory/card_history.js
@@ -13,7 +13,7 @@ router.post('/', async function(req, res, next) {
   function kakaores() {
     return new Promise(function(resolve, reject) {
 
-      kskso_res = response = {
+      kakao_res = response = {
         "version": "2.0",
         "template": {
           "outputs": [{
@@ -23,13 +23,19 @@ router.post('/', async function(req, res, next) {
 
                   "action": "block",
                   "label": "1주일 거래내역",
-                  "blockId": "5fd59318b6158466bd573c77"
+                  "blockId": "5fd59318b6158466bd573c77",
+                  "extra": {
+                    "search_period": "week"
+                  }
                 },
                 {
 
                   "action": "block",
                   "label": "1개월 거래내역",
-                  "blockId": "5fd59321896ca73e8efa7f96"
+                  "blockId": "5fd59321896ca73e8efa7f96",
+                  "extra": {
+                    "search_period": "month"
+                  }
                 },
                 {
 
@@ -54,10 +60,57 @@ router.post('/', async function(req, res, next) {
   }
 });
 
+router.post('/week_history', async function(req, res, next) {
+  function kakaores() {
+    return new Promise(function(resolve, reject) {
+      var fincardNo = "00829101234560000112345678919";
+      let req_Header = {
+        date: moment().format("YYYYMMDD"),
+        hour: moment().format("HHmmss"),
+        start_date: moment().substract(7, 'days').format("YYYYMMDD"),
+        fincardNo: fincardNo
+      };
+      request.post(req_module.InquireCreditCardAuthorizationHistory(req_Header), function(error, response, body) {
+        if (error) {
+          console.error(error);
+        } else {
+          // data = JSON.parse(body);
+          console.log("조회 총 건수",body.Iqtcnt);
+          console.log("거래내역목록", body.REC);
+          console.log("첫번째 목록", body.REC[0]);
 
+          console.log(typeof(body));
+
+          // fs.writeFileSync('public/img/out.png', text2png('잔액\n' + body.Ldbl + "원", {
+          //   font: '80px Futura',
+          //   color: 'teal',
+          //   backgroundColor: 'linen',
+          //   lineSpacing: 10,
+          //   padding: 20
+          // }))
+
+          kakao_res = {
+            "version": "2.0",
+            "template": {
+              "outputs": [{
+                "simpleText": {
+                  "text": "카드거래내역 조회를 선택하셨습니다."
+                }
+
+              }]
+            }
+          }
+          res.status(200).send(kakao_res);
+          kakao_res = {};
+        }
+
+      });
+
+    });
+  }
+});
 
 router.post('/history', async function(req, res, next) {
-  req.body
   let req_Header = {
     date: moment().format("YYYYMMDD"),
     hour: moment().format("HHmmss")
--- routes/req_nh.js
+++ routes/req_nh.js
@@ -9,20 +9,20 @@ module.exports.CheckOpenFinAccountDirect = function(req_Header) {
       "Content-Type": "application/json",
     },
     body: {
-        "Header": {
-          "ApiNm": "CheckOpenFinAccountDirect",
-          "Tsymd": req_Header.date,
-          "Trtm": req_Header.hour,
-          "Iscd": "000553",
-          "FintechApsno": "001",
-          "ApiSvcCd": "DrawingTransferA",
-          "IsTuno": req_Header.date + req_Header.hour + "15975", // user_id 20자리
-          "AccessToken": "55bacf8f744b2e6ff6f01bb2acc0b25a50cf91475dea7c7950e11e02c8a169d4"
-        },
-        "Rgno": "20201212000001052",
-        "BrdtBrno": "19920927"
+      "Header": {
+        "ApiNm": "CheckOpenFinAccountDirect",
+        "Tsymd": req_Header.date,
+        "Trtm": req_Header.hour,
+        "Iscd": "000553",
+        "FintechApsno": "001",
+        "ApiSvcCd": "DrawingTransferA",
+        "IsTuno": req_Header.date + req_Header.hour + "15975", // user_id 20자리
+        "AccessToken": "55bacf8f744b2e6ff6f01bb2acc0b25a50cf91475dea7c7950e11e02c8a169d4"
       },
-      json: true
+      "Rgno": "20201212000001052",
+      "BrdtBrno": "19920927"
+    },
+    json: true
   }
   return req_set
 }
@@ -35,19 +35,19 @@ module.exports.InquireBalance = function(req_Header, req_users) {
       "Content-Type": "application/json",
     },
     body: {
-        "Header": {
-          "ApiNm": "InquireBalance",
-          "Tsymd": req_Header.date,
-          "Trtm": req_Header.hour,
-          "Iscd": "000553",
-          "FintechApsno": "001",
-          "ApiSvcCd": "DrawingTransferA",
-          "IsTuno": req_Header.date + req_Header.hour + "78459", // user_id 20자리
-          "AccessToken": "55bacf8f744b2e6ff6f01bb2acc0b25a50cf91475dea7c7950e11e02c8a169d4"
-        },
-        "FinAcno" : "00820100005530000000000005173"
+      "Header": {
+        "ApiNm": "InquireBalance",
+        "Tsymd": req_Header.date,
+        "Trtm": req_Header.hour,
+        "Iscd": "000553",
+        "FintechApsno": "001",
+        "ApiSvcCd": "DrawingTransferA",
+        "IsTuno": req_Header.date + req_Header.hour + "78459", // user_id 20자리
+        "AccessToken": "55bacf8f744b2e6ff6f01bb2acc0b25a50cf91475dea7c7950e11e02c8a169d4"
       },
-      json: true
+      "FinAcno": "00820100005530000000000005173"
+    },
+    json: true
   }
   return req_set
 }
@@ -60,20 +60,20 @@ module.exports.requestOpenFinCardDirect = function(req_Header) {
       "Content-Type": "application/json",
     },
     body: {
-        "Header": {
-          "ApiNm": "CheckOpenFinCardDirect",
-          "Tsymd": req_Header.date,
-          "Trtm": req_Header.hour,
-          "Iscd": "000524",
-          "FintechApsno": "001",
-          "ApiSvcCd": "DrawingTransferA",
-          "IsTuno": req_Header.date + req_Header.hour + "33333", // user_id 20자리
-          "AccessToken": "55bacf8f744b2e6ff6f01bb2acc0b25a50cf91475dea7c7950e11e02c8a169d4"
-        },
-        "Cano": "9411123456782718", //윤재 카드 번호, 하늘형 카드번호로 수정해야하?
-        "BrdtBrno": "19920927"
+      "Header": {
+        "ApiNm": "CheckOpenFinCardDirect",
+        "Tsymd": req_Header.date,
+        "Trtm": req_Header.hour,
+        "Iscd": "000524",
+        "FintechApsno": "001",
+        "ApiSvcCd": "DrawingTransferA",
+        "IsTuno": req_Header.date + req_Header.hour + "33333", // user_id 20자리
+        "AccessToken": "55bacf8f744b2e6ff6f01bb2acc0b25a50cf91475dea7c7950e11e02c8a169d4"
       },
-      json: true
+      "Cano": "9411123456782718", //윤재 카드 번호, 하늘형 카드번호로 수정해야하?
+      "BrdtBrno": "19920927"
+    },
+    json: true
   }
   return req_set
 }
@@ -88,20 +88,50 @@ module.exports.CheckOpenFinCardDirect = function(req_Header) {
       "Content-Type": "application/json",
     },
     body: {
-        "Header": {
-          "ApiNm": "CheckOpenFinCardDirect",
-          "Tsymd": req_Header.date,
-          "Trtm": req_Header.hour,
-          "Iscd": "000524",
-          "FintechApsno": "001",
-          "ApiSvcCd": "DrawingTransferA",
-          "IsTuno": req_Header.date + req_Header.hour + "33334", // user_id 20자리
-          "AccessToken": "55bacf8f744b2e6ff6f01bb2acc0b25a50cf91475dea7c7950e11e02c8a169d4"
-        },
-        "Rgno": "20201212000001052", //수정해야 하늘형꺼 등록번호로?
-        "BrdtBrno": "19920927"
+      "Header": {
+        "ApiNm": "CheckOpenFinCardDirect",
+        "Tsymd": req_Header.date,
+        "Trtm": req_Header.hour,
+        "Iscd": "000524",
+        "FintechApsno": "001",
+        "ApiSvcCd": "DrawingTransferA",
+        "IsTuno": req_Header.date + req_Header.hour + "33334", // user_id 20자리
+        "AccessToken": "55bacf8f744b2e6ff6f01bb2acc0b25a50cf91475dea7c7950e11e02c8a169d4"
       },
-      json: true
+      "Rgno": "20201212000001052", //수정해야 하늘형꺼 등록번호로?
+      "BrdtBrno": "19920927"
+    },
+    json: true
+  }
+  return req_set
+}
+
+module.exports.InquireCreditCardAuthorizationHistory = function(req_Header) {
+  req_set = {
+    uri: nh_url + '/InquireCreditCardAuthorizationHistory.nh',
+    headers: {
+      "accept": "application/json",
+      "Content-Type": "application/json",
+    },
+    body: {
+      "Header": {
+        "ApiNm": "CheckOpenFinCardDirect",
+        "Tsymd": req_Header.date,
+        "Trtm": req_Header.hour,
+        "Iscd": "000524",
+        "FintechApsno": "001",
+        "ApiSvcCd": "DrawingTransferA",
+        "IsTuno": req_Header.date + req_Header.hour + "35555", // user_id 20자리
+        "AccessToken": "e33d042424e78f837d716eeda26933d030f2e9a0b965d543ceaca7597ed2cbba"
+      },
+      "FinCard": "00829101234560000112345678919",
+      "IousDsnc": "1",
+      "Insymd": "20191105",
+      "Ineymd": "20191109",
+      "PageNo": "1",
+      "Dmcnt": "15"
+    },
+    json: true
   }
   return req_set
 }
